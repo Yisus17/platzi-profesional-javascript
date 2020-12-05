3f09593944a5bd7271974c068040280b7e4c21bc
Jesus Arevalo <jarevalo@moddo.com>
2020-12-05 16:17:23 +0100

    [TypeScript]: Tipos de datos

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,41 @@
-console.log("Hello, TypeScript");
+//Boolean
+let muted: boolean = true;
+muted = false;
+//muted = "callado" ;//-->Error
 
-function add(a: number, b:number) {
-  return a + b;
+//Números
+let numerador: number = 42;
+let denominador: number = 6;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = "Jesús";
+let saludo = `Me llamo ${nombre}`;
+
+//Arreglos
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+//people.push(900); //-->Error de tipo
+people.push("900"); //ok
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push("Jesús");
+peopleAndNumbers.push(900);
+
+//Enum
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul",
+    Amarillo = "Amarillo"
 }
 
-const sum = add(2,3);
+let colorFavorito : Color = Color.Amarillo;
+console.log(`Mi color favorito es: ${colorFavorito}`);
+
+//Any
+let comodin: any = "Joker";
+comodin = {type: "WildCard"};
+
+//Object
+let someObject: object = {type: "WildCard"};
