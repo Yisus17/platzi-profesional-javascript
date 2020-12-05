f4d4d71fee7e59cd6aa3023d4bb2e614b23a5873
Jesus Arevalo <jarevalo@moddo.com>
2020-12-05 16:36:32 +0100

    [TypeScript]: Funciones

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -24,18 +24,42 @@ peopleAndNumbers.push(900);
 
 //Enum
 enum Color {
-    Rojo = "Rojo",
-    Verde = "Verde",
-    Azul = "Azul",
-    Amarillo = "Amarillo"
+  Rojo = "Rojo",
+  Verde = "Verde",
+  Azul = "Azul",
+  Amarillo = "Amarillo",
 }
 
-let colorFavorito : Color = Color.Amarillo;
-console.log(`Mi color favorito es: ${colorFavorito}`);
+let colorFavorito: Color = Color.Amarillo;
+//console.log(`Mi color favorito es: ${colorFavorito}`);
 
 //Any
 let comodin: any = "Joker";
-comodin = {type: "WildCard"};
+comodin = { type: "WildCard" };
 
 //Object
-let someObject: object = {type: "WildCard"};
+let someObject: object = { type: "WildCard" };
+
+/*********************Funciones*********************/
+function add(a: number, b: number): number {
+  return a + b;
+}
+const sum = add(4, 6);
+
+//Una funcion que retorna otra funcion, con el arrow lo indicamos
+function createAdder(a: number): (number) => number {
+  return function (b: number) {
+    return b + a;
+  };
+}
+const addFour = createAdder(4);
+const fourPlus = addFour(6);
+
+//funcion con parametros opcionales "?" el argumento puede venir undefined o con
+//un valor por omision, o incluso poder marcarlo por defecto en caso de que falte
+function fullName(firstName: string, lastName: string = 'Perez'): string {
+  return `${firstName} ${lastName}`;
+}
+//const jesus = fullName('Jesus', 'Arevalo');
+const jesus = fullName("Jesus");
+console.log(jesus);
